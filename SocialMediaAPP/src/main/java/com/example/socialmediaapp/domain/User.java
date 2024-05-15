151b1368f77de7a8ae485d050d062fcb49bdad27
moon110598096 <19980405@gmail.com>
2024-05-16 00:25:19 +0800

    Update User entity field

--- SocialMediaAPP/src/main/java/com/example/socialmediaapp/domain/User.java
+++ SocialMediaAPP/src/main/java/com/example/socialmediaapp/domain/User.java
@@ -1,28 +1,26 @@
 package com.example.socialmediaapp.domain;
 
 public class User {
-    private String phoneNumber;
-    private int userID;
+    private String userId;
     private String userName;
     private String email;
     private String password;
     private String biography;
 
-    public User(String phoneNumber, int userID, String userName, String email, String password, String biography) {
-        this.phoneNumber = phoneNumber;
-        this.userID = userID;
+    public User(String phoneNumber, String userName, String email, String password, String biography) {
+        this.userId = phoneNumber;
         this.userName = userName;
         this.email = email;
         this.password = password;
         this.biography = biography;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public User() {
+
     }
 
-    public int getUserID() {
-        return userID;
+    public String getUserId() {
+        return userId;
     }
 
     public String getUserName() {
