3141fb8901806df0487b0bbbeee04921376f875c
moon110598096 <19980405@gmail.com>
2024-05-17 22:20:47 +0800

    add button

--- SocialMediaAPP/frontEnd/vue/src/views/PostView.vue
+++ SocialMediaAPP/frontEnd/vue/src/views/PostView.vue
@@ -31,7 +31,28 @@ const refreshSubmit = async () => {
     alert('發表失敗');
   }
 }
+const commentSubmit = async (postID) => {
 
+}
+const EditSubmit = async (postID) => {
+
+}
+const DelSubmit = async (postID) => {
+  axios.defaults.baseURL = 'http://localhost:8080'
+  try{
+  const response = await axios.delete(
+      'api/article/article/', {postID});
+
+  if (response.data === true) {
+    alert('刪除成功');
+  } else {
+    alert('刪除失敗');
+  }
+} catch (error) {
+  console.error('失敗', error);
+  alert('刪除失敗');
+}
+}
 const postSubmit = async () => {
   try {
     axios.defaults.baseURL = 'http://localhost:8080'
@@ -68,9 +89,11 @@ const postSubmit = async () => {
       <el-form-item class="refresh-form-button">
         <el-button type="primary" @click="refreshSubmit">列出所有發文!</el-button>
       </el-form-item>
-      <li v-for="item in historyForm.postList">
-        {{ item.authorID }}:
-        {{ item.content }}
+      <li v-for="(item, index) in historyForm.postList">
+        {{ item.authorID }}:{{ item.content }}
+        <el-button type="primary" @click="commentSubmit(item.articleID)">留言</el-button>
+        <el-button type="primary" @click="EditSubmit(item.articleID)">修改</el-button>
+        <el-button type="primary" @click="DelSubmit(item.articleID)">刪除</el-button>
       </li>
     </el-form>
   </div>
