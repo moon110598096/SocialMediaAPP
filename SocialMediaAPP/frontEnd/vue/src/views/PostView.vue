0b838234aa7f60bc7d82e09b99263e2758bb72b5
moon110598096 <19980405@gmail.com>
2024-05-17 21:14:47 +0800

    add login and post feature

--- SocialMediaAPP/frontEnd/vue/src/views/PostView.vue
+++ SocialMediaAPP/frontEnd/vue/src/views/PostView.vue
@@ -4,12 +4,35 @@ import {ElFormItem} from 'element-plus'
 import {ElButton} from 'element-plus'
 import {ElInput} from 'element-plus'
 import { reactive } from 'vue'
-import {RouterLink} from "vue-router";
+import {RouterLink, useRoute} from "vue-router";
+import axios from "axios";
+import router from "@/router";
+
+const route = useRoute();
+let userId = route.params.userID;
+let loginChecked = route.params.userChecked;
+
 const form = reactive ({
-  post : '',
+  postContent : '',
 })
-const onLoginSubmit = () => {
-  console.log('Login!')
+const postSubmit = async () => {
+  try {
+    axios.defaults.baseURL = 'http://localhost:8080'
+    const response = await axios.post(
+        'api/article/article', {
+          authorID:userId,
+          content:form.postContent,
+        });
+
+    if (response.data === true) {
+      alert('發表成功');
+    } else {
+      alert('發表失敗');
+    }
+  } catch (error) {
+    console.error('失敗', error);
+    alert('發表失敗');
+  }
 }
 
 </script>
@@ -18,10 +41,10 @@ const onLoginSubmit = () => {
   <div class="login">
     <el-form :model="form" label-width="auto" style="max-width: 600px">
       <el-form-item label="Post">
-        <el-input v-model="form.post" />
+        <el-input v-model="form.postContent" />
       </el-form-item>
       <el-form-item class="form-button">
-        <RouterLink to="/comment">Comment</RouterLink>
+        <el-button type="primary" @click="postSubmit">發表!</el-button>
       </el-form-item>
     </el-form>
   </div>
