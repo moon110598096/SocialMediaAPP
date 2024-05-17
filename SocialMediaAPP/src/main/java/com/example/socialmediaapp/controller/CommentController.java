c8cf8c07ffc3084364b5793c8cc8b2c4eb9375b5
moon110598096 <19980405@gmail.com>
2024-05-17 17:21:44 +0800

    add comment controller api config

--- SocialMediaAPP/src/main/java/com/example/socialmediaapp/controller/CommentController.java
+++ SocialMediaAPP/src/main/java/com/example/socialmediaapp/controller/CommentController.java
@@ -1,4 +1,27 @@
 package com.example.socialmediaapp.controller;
 
+import com.example.socialmediaapp.domain.Article;
+import com.example.socialmediaapp.domain.Comment;
+import com.example.socialmediaapp.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/api/comment")
 public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping(path = "/comment")
+    public boolean addComment(@RequestBody Comment comment) {
+        return commentService.addComment(comment);
+    }
+
+    @GetMapping(path = "/comment")
+    public Comment findCommentById(@RequestBody Comment comment) {
+        return commentService.findCommentById(comment);
+    }
 }
